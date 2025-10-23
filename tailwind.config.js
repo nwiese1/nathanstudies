73e5fd6a875283964ecb0bc0899822c2fefbe104
Nathan <nwiese108@gmail.com>
2025-10-23 16:28:52 -0700

    Add Tailwind CSS configuration file
    Configured Tailwind CSS with custom theme settings.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      colors: {
+        main: "#202124",
+      },
+      borderRadius: {
+        xl: "1.5rem",
+        "2xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
